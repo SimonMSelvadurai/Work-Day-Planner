57ccf3b2dbb39bb4cc6b64070a58abf2fe0ba66d
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-06-27 19:29:01 +1000

    code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,42 +10,37 @@ loadStoredAppointments();
 // function will be called once the html page loaded...
 $(document).ready(function () {
   setInterval(displayTime, 1000);
-  time();
+  checkTimeBlock();
 });
+/**
+ * On click - Save button function
+ * Gets the time from the ID(Time) and the tasks
+ * Save it in local storage
+ */
 
 $(".container").on("click", ".saveBtn", function () {
   const element = $(this);
-  console.log(" Time Element : " + element.data("time"));
   const appointmentTime = element.parent().attr("id");
-  // const hourhour = element.parent().attr("time-block");
   var appointmentDetails = element.siblings(".description").val();
-  // var hour = element.siblings("time-block").val();
 
-  var hour = element.parent().attr(".hour");
-  // var timeBlock = $(".time-block");
-  console.log(" hour : = " + hour);
-
+  // checks if the field is empty
   if (appointmentDetails != null && appointmentDetails.length > 0) {
-    console.log(" Text  : = " + appointmentDetails);
-    console.log(" ID : = " + appointmentTime);
-    // console.log(" hour : = " + hour);
-    // var appointmentTime1 = $("#id").val();
-    // console.log(" appointmentTime1 : = " + appointmentTime1);
-
     localStorage.setItem(appointmentTime, appointmentDetails);
+    window.alert("The entered Task(s) has been saved successfully!!!");
   } else {
     window.alert("Please enter a task and press the save button");
   }
 });
-
-function time() {
+/**
+ * Check the current hour with the callender and color code them
+ */
+function checkTimeBlock() {
   var currentTime;
   var currentHour = moment().hour();
-  // currentHour = 10;
+  currentHour = 10;
   var timeBlock = $(".time-block");
   for (var i = 0; i < timeBlock.length; i++) {
     var block = timeBlock[i];
-    // console.log("block >>>>>>>>>>>>>" + block.val());
     if (parseInt(block.id.split("-")[0]) < currentHour) {
       $(block).addClass("past");
     } else if (parseInt(block.id.split("-")[0]) === currentHour) {
@@ -58,14 +53,8 @@ function time() {
     }
   }
 }
-/*
-$(".container").on("click", ".saveBtn", function () {
-  var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
-  var time = $(this).parent().attr("id");
-  console.log(" Text  : = " + text);
-  console.log(" ID : = " + time);
-});
-*/
+
+// Clear the local storege contents...
 function clearLocalStorage() {
   localStorage.clear();
 }
@@ -84,12 +73,13 @@ function loadStoredAppointments() {
   $("#16PM .description").val(localStorage.getItem("16PM"));
   $("#17PM .description").val(localStorage.getItem("17PM"));
 }
+
 //Clear button click listener
 $(".clearBtn").on("click", clearClick);
 
 //Save input to local storage
 function clearClick(event) {
-  var clear = confirm("Are you sure to clear all the appointments");
+  var clear = confirm("Are you sure to clear all the tasks for the day");
 
   if (clear) {
     localStorage.clear();
